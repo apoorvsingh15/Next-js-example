7fec8b5a88b90d0ad43daec71a7f21e2fb1e67eb
Apoorv Singh <apoorv.singh@teamlance.io>
2019-05-02 15:34:07 +0530

    added deployment

--- next.config.js
+++ next.config.js
@@ -3,3 +3,7 @@ const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
   cssModules: true
 })
+
+module.exports = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Next-js-example' : '',
+}
